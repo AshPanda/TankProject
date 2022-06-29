bd102d10dece0e3bb9105cd62bf3bfafe516855f
AshPanda <alena.shwaika@gmail.com>
2022-06-29 14:57:05 +0300

    move to the right

---
+++ TankoGeddon/Source/TankoGeddon/TankPawn.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPawn.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ATankPawn::ATankPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMesh"));
+	RootComponent = BodyMesh;
+
+	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+	TurretMesh->SetupAttachment(BodyMesh);
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(BodyMesh);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+}
+
+void ATankPawn::MoveForward(float Value)
+{
+	TargetForwardAxisValue = Value;
+}
+
+void ATankPawn::MoveRight(float Value)
+{
+	TargetRightAxisValue = Value;
+}
+
+
+
+
+void ATankPawn::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	FVector CurrentLocation = GetActorLocation();
+	FVector forwardVector = GetActorForwardVector();
+	FVector rightVector = GetActorRightVector();
+	FVector movePosition = CurrentLocation + (forwardVector * MoveSpeed * TargetForwardAxisValue) + (rightVector * MoveSpeed * TargetRightAxisValue);
+	SetActorLocation(movePosition, true);
+
+	
+
+}
+// Called when the game starts or when spawned
+void ATankPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	SetActorLocation(FVector(GetActorLocation().X, GetActorLocation().Y, 0.0f));
+}
+
+void ATankPawn::RotateRight(float Value)
+{
+
+}
+
+// Called to bind functionality to input
+void ATankPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
